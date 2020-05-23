fde11a45ea8306743658b88d2f656b1e4c483478
dreamlot <wny001@gmail.com>
2020-05-22 20:07:03 -0500

    Output oilblob colored by curvature radius
    There is always one or several points in the high curvature zone with strange curvature radius value. Need to further check the segmentation and interpolation before the curvature computation.

--- src/findCurvatureRadii.py
+++ src/findCurvatureRadii.py
@@ -2,7 +2,7 @@
 """
 This program is to find the curvature radii along the oil blob perimeter.
 It studies only the oil blob on the left hand side.
-The figure will be cut in half and the right hand side half image would 
+The figure will be cut in half and the right hand side half image would
 be abandoned.
 
 
@@ -11,24 +11,30 @@ Created on Thu Feb 27 21:27:19 2020
 @author: ningyu
 """
 
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as optimize
 
+# This implies a code from internet. Reference see inside.
 from findCurvatureRadii2 import findCurvatureR
 
 # show image
+from imshow import imshow
+
+'''
+# show image
 def imshow(img,showresult=True,name='result',x=800,y=600):
-    if not showresult: 
+    if not showresult:
         return
 
-    cv2.namedWindow(str(name),cv2.WINDOW_NORMAL); 
+    cv2.namedWindow(str(name),cv2.WINDOW_NORMAL);
     cv2.imshow(str(name),img);
     cv2.resizeWindow(str(name), x,y);
     cv2.waitKey(0);
     cv2.destroyAllWindows();
-    
+'''
 # Interpolate the contour for long straight sections
 def interpolateContour(points,insert=1,distancetol=5):
     points = np.asarray(points)
@@ -44,7 +50,7 @@ def interpolateContour(points,insert=1,distancetol=5):
             y = a[1] * (insert-j) / insert + b[1] * j / insert
             points = np.insert(points,n,[x,y],axis=0)
     '''
-    
+
     # then, deall with other points
     for i in range(n-1,0,-1):
         d = np.linalg.norm(points[i,:]-points[i-1,:]);
@@ -55,39 +61,42 @@ def interpolateContour(points,insert=1,distancetol=5):
                 x = a[0] * (insert-j) / insert + b[0] * j / insert
                 y = a[1] * (insert-j) / insert + b[1] * j / insert
                 points = np.insert(points,i,[x,y],axis=0)
-    return(points) 
+    return(points)
 
 # Get the oil blob perimeter
+# Returns the grayscale image and a 2D array of xy coordinates
+# of points on the contour.
 def findOilBlob(filename,threshold=127,iterations=[2,8,6],showresult=False):
     #filename = "test.jpg"
     print("Reading image : ", filename)
     imReference = cv2.imread(filename, cv2.IMREAD_COLOR)
-    
+
     '''
     # filter image
     imReference = cv2.bilateralFilter(imReference,5,40,40)
     '''
-    
+
     # convert to grayscale
     imgray = cv2.cvtColor(imReference, cv2.COLOR_BGR2GRAY)
+    imgrayorig = imgray.copy()
     #imgSPLIT = cv2.split(imReference);
     #imgray = imgSPLIT[1];
     imshow(imgray,showresult)
-    
+
     # filter image
     imgray = cv2.bilateralFilter(imgray,5,40,40)
-    
-    # cut half of the image 
+
+    # cut half of the image
     # use only the LHS
     imgray = imgray[:,0:int(imgray.shape[1]/2)];
-    
+
     # cut image edges
     imReference = imReference[2:-2,2:-2,:]
-    
-    
+
+
     # denoise
     imgray = cv2.fastNlMeansDenoising(imgray,None,10,7,21);
-    
+
     # erode and dilate
     kernel = np.ones((5,5),np.uint8)
     dilation = cv2.dilate(imgray,kernel,iterations = iterations[0])
@@ -95,23 +104,29 @@ def findOilBlob(filename,threshold=127,iterations=[2,8,6],showresult=False):
     imshow(erosion,showresult);
     dilation = cv2.dilate(erosion,kernel,iterations = iterations[2])
     imshow(dilation,showresult);
-    
+
     # get the contours
     #ret, thresh = cv2.threshold(dilation, 127, 255, 0)
     ret, thresh = cv2.threshold(dilation, 127, 255, cv2.THRESH_OTSU)
+    imshow(thresh)
+    '''
+    # The cv2.findContours() function removed the first output.
     tmpim, contours, hierarchy = cv2.findContours(thresh, method=cv2.RETR_TREE, \
                                               mode=cv2.CHAIN_APPROX_SIMPLE)
+    '''
+    contours, hierarchy = cv2.findContours(thresh, method=cv2.RETR_TREE, \
+                                       mode=cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(imgray, contours, contourIdx = -1, color=(255,0,0), \
                      thickness=4)
     imshow(imgray,showresult);
-    
-    # First, the real interfaces have a lot of points. 
+
+    # First, the real interfaces have a lot of points.
     # Abandon the contours with only several points.
     # Keep only the longest contours.
     contours = sorted(contours,key=len);
     contours = contours[0:3];
-    
-    # Then, there are three longest contours found: 
+
+    # Then, there are three longest contours found:
     # 1. the upper glass, average y value largest
     # 2. the lower glass, average y value smallest
     # 3. the oil blob, average y values in the middle
@@ -131,8 +146,8 @@ def findOilBlob(filename,threshold=127,iterations=[2,8,6],showresult=False):
         y[i] = np.mean(contours[i][:,:,1])
     print(y)
     '''
-    
-    return (imgray,contours)
+
+    return (imgray,contours,imgrayorig)
 
 # Function of a circle
 def funcCircle(para,x,y):
@@ -159,28 +174,41 @@ def findCircle(points):
                                   xtol = 1e-12,bounds=bounds,args=(x,y)).x)
 
 # find Curvature
+# return x,y,r of the inscribed circle at each point
+# NW modify on May22-2020 to locate the window around the point
+# instead of after it.
 def findCurvatureRadius(points,window=5):
     # number of points
     N = points.shape[0];
-    
-    # window length used to fit the circle
-    n = window;
-    
-    print(N)
+
+    # half window length used to fit the circle
+    n = int(window/2);
+
+    #print(N)
     # fitted circle
     result = np.zeros([N,3]);
     
-    for i in range(N):
-        if i < N-n:
-            indx = np.arange(i,(i+n))
-        else:
-            indx = np.concatenate((np.arange(i,N),np.arange(0,i+n-N)))
-        #result[i,:] = findCircle(points[indx,:]);
-        x = points[indx,0]
-        y = points[indx,1]
-        result[i,:] = findCurvatureR(x,y)
+    try:
+        for i in range(N):
+            if i < n+1:
+                indx = np.concatenate((np.arange(N+i-n,N), np.arange(0,i+n+1)))
+            elif i + n >= N:
+                indx = np.concatenate((np.arange(i-n,N), np.arange(0,i+n-N)))
+            else:
+                indx = np.arange((i-n),(i+n+1))
+                
+            #result[i,:] = findCircle(points[indx,:]);
+            x = points[indx,0]
+            y = points[indx,1]
+            result[i,:] = findCurvatureR(x,y)
+    except:
+        print(i,n,N)
+        print(N+i-n,i+n+1)
+        print(i-n,i+n-N)
+        
+            
     return( result )
-    
+
 
 
 # Function of a ellipse
@@ -237,7 +265,7 @@ def atan(x,y,x0=0,y0=0):
     y = y-y0;
     if n != len(y):
         raise('The length of x and length of y should be the same!')
-    
+
     theta = np.zeros(n);
     for i in range(n):
         if x[i] == 0 and y[i] == 0:
@@ -250,15 +278,15 @@ def atan(x,y,x0=0,y0=0):
             theta[i] = 0
         elif y[i] == 0 and x[i] < 0:
             theta[i] = np.pi
-            
+
         elif x[i] >= 0 and y[i] >= 0:
             theta[i] = np.arctan(y[i]/x[i]);
         elif x[i] >= 0 and y[i] < 0:
             theta[i] = 2 * np.pi - np.arctan(abs(y[i]/x[i]));
         elif x[i] < 0 and y[i] >= 0:
-            theta[i] = np.pi - np.arctan(abs(y[i]/x[i])) 
+            theta[i] = np.pi - np.arctan(abs(y[i]/x[i]))
         elif x[i] < 0 and y[i] < 0:
-            theta[i] = np.pi + np.arctan(abs(y[i]/x[i])) 
+            theta[i] = np.pi + np.arctan(abs(y[i]/x[i]))
     return(theta)
 
 
@@ -284,7 +312,7 @@ def avgFilter(data,window):
             data1[i] = (sum(data[(i-window+1):])+sum(data[0:i+1]))/window
             #print(i)
             #print(data[(i-window+1):],data[0:i+1])
-            
+
         else:
             data1[i] = np.mean(data[(i-window):(i)])
             #print(i-window,i)
@@ -297,60 +325,61 @@ def avgFilter(data,window):
 
 
 # test
-if __name__ == '__main__':
-    workpath = '../test';
+def test1():
+    #workpath = '../test';
+    workpath = 'F:/ferrofluid_experiment/postprocessing/noflow_rotateMag/ts3_1fps';
     #filename = 'ts3_0000010081.tif';
     #filename = 'ts3_0000012692.tif';
     #filename = 'ts3_0000012718.tif';
     #filename = 'ts3_0000010072.tif';
     #filename = 'ts3_0000010158.tif';
     #filename = 'ts3_0000012622.tif';
-    filename = 'Media10000.tif';
+    filename = 'ts3_001005.tif';
     thre = 127;
     iteration = [1,5,4]
     showresult = False;
-    
-    
-    # average filter: 
+
+
+    # average filter:
     # average this number of points to generate a point
     freqratio = 3;
-    
+
     # number of points used to fit the circle
     #window = [18,20,25]
     window = [20]
     #window = [15]
-    
+
 
     filename = workpath +"/" + filename
-    cha1,contours = findOilBlob(filename,iterations=iteration,showresult=showresult);
+    cha1,contours,__ = findOilBlob(filename,iterations=iteration,showresult=showresult);
     #cv2.drawContours(cha1, contours, contourIdx = -1, color=(255,0,0), \
     #                 thickness=4)
     #imshow(cha1)
-    
+
     # shrink unwanted dimension
     oilcontour = contours[1][:,0,:].astype(float);
-    
+
     oilcontour0 = np.copy(oilcontour)
-    
-    
+
+    # save a copy of the original image
     oilorg = np.copy(oilcontour)
-    
+
     # interpolate straight section
     #oilcontour = interpolateContour(oilcontour,insert=freqratio-1,distancetol=30)
     oilcontour = interpolateContour(oilcontour,insert=freqratio-1,distancetol=10)
-    
-    
+
+
     plt.figure()
     plt.plot(oilcontour[:,0],oilcontour[:,1],'bs-')
     plt.plot(oilorg[:,0],oilorg[:,1],'r+')
-    
-    
+
+
     '''
     # low pass filter
     oilcontour[:,0] = butter_lowpass_filter(oilcontour[:,0], cutoff, fs, order)
     oilcontour[:,1] = butter_lowpass_filter(oilcontour[:,1], cutoff, fs, order)
     '''
-    
+
     # filter and subsample
     # averaging filter
     oilcontour[:,0] = avgFilter(oilcontour[:,0], int(freqratio))
@@ -366,31 +395,31 @@ if __name__ == '__main__':
     plt.plot(oilcontour[:,0],oilcontour[:,1],'s',label='subsampled oil contour')
     #plt.axis('square')
     #plt.title('oil blob')
-    
+
     # find the ecliptical fit
     ellps = findEllipse(oilcontour)
-    
+
     theta = np.linspace(0,2*np.pi,len(oilcontour[:,0]))
     X = ellps[2]*np.cos(theta);
     Y = ellps[3]*np.sin(theta);
     x = ellps[0] + X*np.cos(ellps[4])-Y*np.sin(ellps[4])
     y = ellps[1] + X*np.sin(ellps[4])+Y*np.cos(ellps[4])
-    
+
     plt.plot(x,y,'r-',label='fitted ellipse')
     plt.axis('square')
     plt.title('oil blob',fontsize=12)
     plt.legend(prop={'size': 12})
     plt.xlabel('pixels',fontsize=12)
     plt.ylabel('pixels',fontsize=12)
-    
+
     eclpsxy = np.array([x,y]).transpose()
-    
+
 
     for lp0 in window:
-        
+
         curvatureradius = findCurvatureRadius(oilcontour,window=lp0);
         curvatureeclps = findCurvatureRadius(eclpsxy,window=lp0);
-        
+
         tmpcha = np.copy(cha1)
         for i in range(len(curvatureradius[:,0])):
             #if np.mod(i,10)!=0:
@@ -401,7 +430,7 @@ if __name__ == '__main__':
             except:
                 pass
         plt.imsave(workpath+'/pore'+str(lp0)+'.jpg',tmpcha)
-        
+
         fig = plt.figure()
         plt.plot(eclpsxy[:,0],eclpsxy[:,1])
         for i in range(len(curvatureeclps[:,0])):
@@ -415,22 +444,22 @@ if __name__ == '__main__':
                 pass
         plt.axis('square')
         plt.savefig(workpath+'/ellipse'+str(lp0)+'.jpg')
-        
-        
-        
-        
-        
-            
+
+
+
+
+
+
         # compute the phase angle of each point on the oil-water interface
         theta = atan(oilcontour[:,0],oilcontour[:,1], \
                      (max(oilcontour[:,0])+min(oilcontour[:,0]))/2, \
                      (max(oilcontour[:,1])+min(oilcontour[:,1]))/2)
-        
+
         # compute the phase angle of each point on the oil-water interface
         thetaeclps = atan(eclpsxy[:,0],eclpsxy[:,1], \
                      (max(eclpsxy[:,0])+min(eclpsxy[:,0]))/2, \
                      (max(eclpsxy[:,1])+min(eclpsxy[:,1]))/2)
-   
+
         '''
         plt.figure()
         plt.plot(curvatureradius[:,2],'rs-')
@@ -440,7 +469,7 @@ if __name__ == '__main__':
         plt.ylabel('curvature radius')
         plt.title('radius')
         '''
-        
+
         plt.figure()
         thetaeclps1 = np.roll(thetaeclps,int((freqratio+lp0)/2))
         plt.plot(theta*180/np.pi,curvatureradius[:,2],'rs',label='oil blob')
@@ -450,19 +479,19 @@ if __name__ == '__main__':
         theta1 = np.copy(theta)
         theta1 = np.roll(theta1,int((freqratio-lp0)/2))
         #plt.plot(theta1*180/np.pi, R,'b-',label='analitycal ellipse')
-        
+
         plt.xlabel('phase angle')
         plt.ylabel('curvature radius')
         plt.title('radius')
         plt.legend()
-        
+
         '''
         plt.figure()
         plt.hist(curvatureradius[:,2],bins=10)
         plt.title('radius')
         '''
-        
-        
+
+
         plt.figure()
         # semimajor axis coordinate
         x=(oilcontour[:,0]-ellps[0])*np.cos(ellps[4]) - (oilcontour[:,1]-ellps[1])*np.sin(ellps[4]);
@@ -481,9 +510,101 @@ if __name__ == '__main__':
         plt.xlabel('semi-major axis coordinate')
         plt.legend()
         '''
-        
+
         plt.figure()
         x = (oilcontour[:,0]-ellps[0])*np.cos(ellps[4]) - (oilcontour[:,1]-ellps[1])*np.sin(ellps[4]);
         y = (oilcontour[:,0]-ellps[0])*np.sin(ellps[4]) + (oilcontour[:,1]-ellps[1])*np.cos(ellps[4]);
         plt.plot(x,y,'s-')
+
+
+if __name__ == '__main__':
+    
+    # working directory
+    sourcepath = 'F:/ferrofluid_experiment/postprocessing/noflow_rotateMag/ts3_1fps';
+    targetpath = sourcepath +'/target';
+    
+    # parameters
+    thre = 127;
+    iteration = [1,5,4]
+    showresult = False;
+    dotsize = 1;
+    
+
+    # average filter:
+    # average this number of points to generate a point
+    freqratio = 3;
+
+    # number of points used to fit the circle
+    window = [19]
+    
+    # load the files in the source directory
+    # @Vaibhav
+    # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    from os.path import isfile, join
+    files = [f for f in os.listdir(sourcepath) if isfile(join(sourcepath, f))]
+    
+    # prepare the output directory
+    try:
+        os.mkdir(targetpath)
+    except FileExistsError:
+        pass
+    
+    print('Processing...')
+    for ite in enumerate(files):
+        print(' '+ite[1])
         
+        filename = sourcepath+'/'+ite[1];
+        
+        # get the raw contour
+        cha1,contours,imorig = findOilBlob(filename,iterations=iteration,showresult=showresult);
+        
+        # shrink unwanted dimension
+        oilcontour = contours[1][:,0,:].astype(float);
+        # interpolate straight section
+        oilcontour = interpolateContour(oilcontour,insert=freqratio-1,distancetol=10)
+    
+        # filter and subsample
+        # averaging filter
+        oilcontour[:,0] = avgFilter(oilcontour[:,0], int(freqratio))
+        oilcontour[:,1] = avgFilter(oilcontour[:,1], int(freqratio))
+        # subsample
+        ind = np.arange(0,int(len(oilcontour)/freqratio));
+        oilcontour = oilcontour[ind*freqratio]
+        
+        
+        # compute curvature
+        curvatureradius = findCurvatureRadius(oilcontour,window=window[0]);
+        
+        # recover the three channels of original figure
+        imorig = cv2.cvtColor(imorig,cv2.COLOR_GRAY2BGR)
+        
+        
+        # write points into image
+        '''
+        maxradius = max(curvatureradius[:,2])
+        minradius = min(curvatureradius[:,2])
+        '''
+        maxradius = 250;
+        minradius = 100;
+        numcontourpoint = curvatureradius.shape[0]
+        curvatureradiusplot = (curvatureradius[:,2]-minradius) / (maxradius-minradius) * 256
+        Nx,Ny,__ = imorig.shape
+        for lp1 in range(numcontourpoint):
+            # coordinate index
+            indy = int(oilcontour[lp1,0])
+            indx = int(oilcontour[lp1,1])
+            # color
+            imorig[indx-2:indx+3,indy-2:indy+3,[0,1]] = 0
+            imorig[indx-2:indx+3,indy-2:indy+3,2] = curvatureradiusplot[lp1]
+            '''
+            imorig[indx,indy,:]   = [0,0,int(curvatureradiusplot[lp1])];
+            imorig[indx-1,indy,:] = [0,0,int(curvatureradiusplot[lp1])];
+            imorig[indx+1,indy,:] = [0,0,int(curvatureradiusplot[lp1])];
+            imorig[indx,indy-1,:] = [0,0,int(curvatureradiusplot[lp1])];
+            imorig[indx,indy+1,:] = [0,0,int(curvatureradiusplot[lp1])];
+            '''
+            
+        #imshow(imorig)
+        savename = targetpath+'/'+ite[1]
+        cv2.imwrite(savename,imorig)
+        #break
